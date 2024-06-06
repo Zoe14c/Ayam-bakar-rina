c8d1d6d1707e55d3da3abd5b98defaf3b56bfbda
zoe14c <zulfakaryusuf14@gmail.com>
2024-06-06 15:14:48 +0700

    first commit

---
+++ src/script.js
@@ -0,0 +1,10 @@
+// Toggles Active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu diklik
+document.querySelector(".hamburger-menu").onclick = () => {
+	navbarNav.classList.toggle("active");
+};
+{
+}
+
+// Toggles Menu
